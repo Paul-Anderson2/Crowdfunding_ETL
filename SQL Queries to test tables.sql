797fd95feaf9b639a5b360bba95c6e85289692e1
P Anderson <158597021+Paul-Anderson2@users.noreply.github.com>
2024-05-20 12:24:00 -0500

    Add files via upload

---
+++ SQL Queries to test tables.sql
@@ -0,0 +1,20 @@
+-- To test that the tables in the crowdfunding db were correctly imported
+
+-- Test campaign table
+SELECT *
+From campaign
+
+-- Test category table
+SELECT *
+From category
+
+-- Test contacts table
+SELECT *
+From contacts
+
+
+-- Test subcategory table
+SELECT *
+From subcategory 
+
+
